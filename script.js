cfeacca5687e28f6781162cd9eb22773d57af97f
Dvd <dvdnet22@gmail.com>
2021-09-22 09:05:00 -0500

    Replaced the images, probably will make a mess as I didnt test them

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-const selectors = ['.layer', '#github', '#twitter', '#roblox', '#discord']
+const selectors = [".layer", "#github", "#twitter", "#roblox", "#discord"];
 
 document.addEventListener("mousemove", parallax);
 
 function parallax(e) {
-    this.querySelectorAll(selectors).forEach(Layer => {
-        const speed = Layer.getAttribute('data-speed');
+	this.querySelectorAll(selectors).forEach((Layer) => {
+		const speed = Layer.getAttribute("data-speed");
 
-        const x = (window.innerWidth - e.pageX * speed) / 100;
-        const y = (window.innerHeight - e.pageY * speed) / 100;
+		const x = (window.innerWidth - e.pageX * speed) / 100;
+		const y = (window.innerHeight - e.pageY * speed) / 100;
 
-        Layer.style.transform = `translateX(${x}px) translateY(${y}px)`
-    })
+		Layer.style.transform = `translateX(${x}px) translateY(${y}px)`;
+	});
 }
